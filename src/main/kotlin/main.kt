d97b81492d4638d10ebfb6eaf59c5544a991c698
shawn.kuta23 <shawn.kuta23@d155.org>
2022-08-18 07:29:59 -0500

    variable project

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,23 @@
+
+fun main(){
+    //Variable declarations
+    val myInt = 42000
+    val myByte = 120
+    val myShort = 32767
+    val myLong = 10000000000000000
+    val myDouble = 25.298453235823
+    val myFloat = 2.359F
+    val myChar = 'p'
+    val myBoolean = false
+
+    //whole numbers
+    println("Byte: $myByte")
+    println("Short: $myShort")
+    println("Int: $myInt")
+    println("Long: $myLong")
+    //everything else
+    println("Float: $myFloat")
+    println("Double: $myDouble")
+    println("Boolean: $myBoolean")
+    println("Char: $myChar")
+}
